79af3d442756fcaf14b85df26cdce0493a6d6d4f
Marc Ruemekorf <marcruemekorf@users.noreply.github.com>
2025-01-30 16:36:07 +0100

    Configure MDX and add example

--- rd-mdx/src/app/page.tsx
+++ rd-mdx/src/app/page.tsx
@@ -1,10 +1,12 @@
 import styles from "./page.module.css";
+import Welcome from "@/content/welcome.mdx";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Hello World!</h1>
+        <Welcome />
       </main>
     </div>
   );
